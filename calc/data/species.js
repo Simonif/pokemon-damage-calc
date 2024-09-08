4e937dc57e95e42f12fdd7b7ceaa43a88718a10f
Simonif <dunsparce@web.de>
2024-09-08 17:19:01 +0000

    Deploy c57384c

--- calc/data/species.js
+++ calc/data/species.js
@@ -2028,7 +2028,7 @@ var ADV_PATCH = {
     'Ho-Oh': { abilities: { 0: 'Pressure' } },
     Hoothoot: { abilities: { 0: 'Insomnia' } },
     Hoppip: { abilities: { 0: 'Chlorophyll' } },
-    Houndoom: { abilities: { 0: 'Early Bird' } },
+    Houndoom: { abilities: { 0: 'Intimidate' } },
     Houndour: { abilities: { 0: 'Early Bird' } },
     Igglybuff: { abilities: { 0: 'Cute Charm' } },
     Jumpluff: { abilities: { 0: 'Chlorophyll' } },
