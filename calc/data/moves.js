627d6cc6387c33214104ba9f0fd6c5391b745ad5
Simonif <dunsparce@web.de>
2024-10-13 11:27:58 +0000

    Deploy 0a18887

--- calc/data/moves.js
+++ calc/data/moves.js
@@ -477,7 +477,7 @@ var ADV_PATCH = {
     'Icicle Spear': { bp: 10, type: 'Ice', multihit: [2, 5] },
     Ingrain: { bp: 0, category: 'Status', type: 'Grass' },
     'Knock Off': { bp: 20, type: 'Dark', makesContact: true },
-    'Leaf Blade': { bp: 70, type: 'Grass', makesContact: true },
+    'Leaf Blade': { bp: 85, type: 'Grass', makesContact: true },
     'Magic Coat': { bp: 0, category: 'Status', type: 'Psychic', priority: 4 },
     Memento: { bp: 0, category: 'Status', type: 'Dark' },
     'Nature Power': { bp: 0, category: 'Status', type: 'Normal' },
