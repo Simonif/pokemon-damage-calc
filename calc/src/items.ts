8ed36a494ec14c6a8e1b7ec9156591d2ada39976
Simonif <dunsparce@web.de>
2025-04-22 22:19:46 +0200

    Update items.ts

--- calc/src/items.ts
+++ calc/src/items.ts
@@ -26,7 +26,7 @@ export function getItemBoostType(item: string | undefined) {
   case 'Charcoal':
     return 'Fire';
   case 'Icicle Plate':
-  case 'Never-Melt ice':
+  case 'Never-Melt Ice':
     return 'Ice';
   case 'Insect Plate':
   case 'Silver Powder':
